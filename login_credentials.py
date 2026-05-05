3642d5314e7f35bc611695823b27ef3a8575f3e4
saantanapgs <rpggustavo56@gmail.com>
2026-05-05 11:29:12 -0300

    Tracing the selenium to automatizate more proccesses

--- login_credentials.py
+++
@@ -1,6 +0,0 @@
-# ///////////
-#REMOVE THIS FILE FROM THE GIT COMMIT
-# ///////////
-def login_credentials():
-  user = "p"
-  password = "123456"
--- scripts/selenium_navigation.py
+++ scripts/selenium_navigation.py
@@ -2,28 +2,33 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import keys
-from login_credentials import login_credentials
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from login_credentials import user, password
 
 # Definindo o navegador
-driver = webdriver.Chrome()
+options = Options()
+options.add_argument(r"C:\Users\Cemep.sejuc\AppData\Local\Google\Chrome\User Data")
+# Definindo perfil do Chrome
+options.add_argument("profile-directory=Profile 1")
+driver = webdriver.Chrome(options=options)
 
 # Adicionando um delay para realizar cada passo da automação
 wait = WebDriverWait(driver, 10)
 
 driver.get("https://chronos.synergye.com.br/index.php?r=site/login")
 
-user = wait.until(
+login_user = wait.until(
   EC.presence_of_element_located((By.ID, "LoginForm_username"))
-)
+).send_keys(user)
 
-password = wait.until(
+login_password = wait.until(
   EC.presence_of_element_located((By.ID, "LoginForm_password"))
+).send_keys(password)
+
+btn = wait.until(
+    EC.element_to_be_clickable((By.XPATH,"//input[@type='submit']"))
 )
+btn.click()
 
-user, password = login_credentials()
-
-login_user.send_keys(user)
-login_password.send_keys(password)
-
-
+input("23")
